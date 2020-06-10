27738868448dcd9ee5daa695e1d14780e7dfcf36
meliwi <melissa.gonzalez@correounivalle.edu.co>
2020-06-09 23:10:21 -0500

    changes in the use of state

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -4,8 +4,16 @@ import {Navbar, NavbarBrand} from 'reactstrap';
 //importamos el componente que acabamos de crear 
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    //state siempre va dentro del constructor, traemos el estado de dishes
+    this.state={
+      dishes:DISHES
+    };
+  }
   render(){
     return (
       <div className="App">
@@ -16,7 +24,7 @@ class App extends Component{
           </NavbarBrand>
         </div> 
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
