0b5a37345d84b705bb478ba862f3728bd2f96421
meliwi <melissa.gonzalez@correounivalle.edu.co>
2020-06-16 21:11:58 -0500

    futncional components

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,18 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card,CardImgOverlay,CardText,CardBody,CardTitle, CardImg} from 'reactstrap';
 
-
+    function rederMenuItem({dish,onClick}){
+        return(
+            <Card onClick={()=>this.props.onClick(dish.id)}>
+            <CardImg width="100%" object src={dish.image} alt = {dish.name}/>
+            <CardImgOverlay body className="ml-5">
+                <CardTitle >
+                    {dish.name}
+                </CardTitle>
+            </CardImgOverlay>
+        </Card>
+        );
+    }
         const menu = this.props.dishes.map((dish)=>{
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={()=>this.props.onClick(dish.id)}>
-                        <CardImg width="100%" object src={dish.image} alt = {dish.name}/>
-                        <CardImgOverlay body className="ml-5">
-                            <CardTitle >
-                                {dish.name}
-                            </CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+
                 </div>
             );
         });
