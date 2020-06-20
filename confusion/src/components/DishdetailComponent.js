ba1c565d194d5857ff3bb7c656eb48af9c81fece
meliwi <melissa.gonzalez@correounivalle.edu.co>
2020-06-19 21:32:03 -0500

    single page application part 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Card,CardText, CardImgOverlay,CardBody,CardTitle, CardImg} from 'reactstrap';
-
+import {Card,CardText, CardImgOverlay,CardBody,CardTitle, CardImg,Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
     function RenderDish({dish}) {
         return(
@@ -14,45 +14,59 @@ import {Card,CardText, CardImgOverlay,CardBody,CardTitle, CardImg} from 'reactst
         );
       }
       //Now we change the format of the hour 
-      function RenderComments({comments}){
-          return(
-              <ul className="list-unstyled">
-                  {comments.map((comment)=>{
-                      return(
-                          <li key={comment.id}>
-                              <p>
-                                  {comment.comment}
-                              </p>
-                              <p>
-                                  -- {comment.author},
+    
+     function RenderComments({comments}){
+         if(comments!=null)
+         return(
+            <div className="col-12 col-md--5 m-1">
+                <h4>
+                    Comments
+                </h4>
+                <ul className="list-unstyled"> 
+                    {comments.map((comment)=>{
+                        return(
+                            <li key={comment.id}>
+                                <p>
+                                    {comment.comment}
+                                </p>
+                                <p>
+                                -- {comment.author},
                                   {new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
-                              </p>
-                              
-                          </li>
-                      );
-                  })}
-              </ul>
-          );
-      }
+                                </p>
+                            </li>
+                        );
+                     }
+                    )}
+                </ul>
+            </div>
+         );
+     }
 
     //the render function now is a function component
     const DishDetail =(props) =>  {
         console.log('Dishdetail Component render invoked');
         if (props.dish!=null){
-            return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish}/>
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>
-                            Comments
-                        </h4>
-                        <RenderComments comments ={props.dish.comments}/>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
                     </div>
                 </div>
-            </div>
             );
         }
         else{
