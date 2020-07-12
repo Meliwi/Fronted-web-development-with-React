fa4a4227ece9da6b81da799bbc003b522e8a13bf
meliwi <melissa.gonzalez@correounivalle.edu.co>
2020-07-12 13:14:21 -0500

    combining reducers

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//importamos el componente que acabamos de crear 
+//importamos el componente principal que acabamos de crear 
 import Main from './components/MainComponent';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
