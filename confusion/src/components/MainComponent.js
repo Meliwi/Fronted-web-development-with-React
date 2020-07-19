354807b8899da8a91ff29a3201f32e9793a33b72
meliwi <melissa.gonzalez@correounivalle.edu.co>
2020-07-18 21:10:10 -0500

    almost done react redux form

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import {Switch,Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {addComment,fetchDishes } from '../redux/ActionCreators';
+import {actions} from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -22,7 +23,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author,comment) => dispatch(addComment(dishId, rating, author,comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 });
 
 
